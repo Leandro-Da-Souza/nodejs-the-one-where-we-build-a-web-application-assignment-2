7cde6d89c84f382a7c97ca7a5a8a5096aac963e5
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-02-03 11:26:55 +0100

    finished individual product page and add to cart functionality, create a cartpage.html and cart.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 let productPage = document.querySelector('.products ul');
 let baseURL = 'http://localhost:8080';
+let customerID = 'f6a796d0-3f60-11ea-80cd-8bb22b23e2bf';
+let counter = document.querySelector('#counter');
+
+const fetchBasket = async custID => {
+    let response = await fetch(`${baseURL}/getBasket?customerID=${custID}`, {
+        method: 'GET'
+    });
+
+    let data = response.json();
+    return data;
+};
 
 const fetchProducts = async () => {
     let response = await fetch(`${baseURL}/products`, {
@@ -12,10 +23,12 @@ const fetchProducts = async () => {
 
 window.addEventListener('load', async () => {
     let products = await fetchProducts();
+    let basket = await fetchBasket(customerID);
     let output = '';
 
     products.forEach(product => {
         console.log(product);
+
         output += `
         <li>
             <h4>${product.name}</h4>
@@ -25,4 +38,6 @@ window.addEventListener('load', async () => {
 
         productPage.innerHTML = output;
     });
+
+    counter.innerHTML = basket.length + ' Items In Basket';
 });
--- js/product.js
+++ js/product.js
@@ -2,6 +2,7 @@ const href = window.location.href;
 const baseURL = 'http://localhost:8080';
 const productID = href.slice(href.indexOf('?') + 4);
 const productPage = document.querySelector('#product');
+const customerID = 'f6a796d0-3f60-11ea-80cd-8bb22b23e2bf';
 
 const getProduct = async id => {
     const data = await fetch(`${baseURL}/products/${id}`, {
@@ -13,15 +14,49 @@ const getProduct = async id => {
     return response;
 };
 
+const addProduct = async (custID, prodID) => {
+    const response = await fetch(
+        `${baseURL}/addToBasket?customerID=${custID}&productID=${prodID}`,
+        { method: 'POST' }
+    );
+
+    let data = await response.json();
+    return data;
+};
+
 window.addEventListener('load', async () => {
     let product = await getProduct(productID);
     let output = `
         <h1>${product.name}</h1>
-        <p>${product.price}</p>
+        <p>${product.price} $$$</p>
         <img src="${product.img}" />
+        <button id="basketAdd">Add To Basket</button>
     `;
 
     productPage.innerHTML = output;
 
     console.log(product);
+});
+
+productPage.addEventListener('click', async e => {
+    console.log(e.target.id);
+    if (e.target.id === 'basketAdd') {
+        let res = await addProduct(customerID, productID);
+        if (res.hasOwnProperty('error')) {
+            console.log('Item Already In Basket');
+            let pElem = document.createElement('p');
+            pElem.innerHTML = 'Product Already In Basket';
+            productPage.appendChild(pElem);
+            setTimeout(() => {
+                productPage.removeChild(pElem);
+            }, 3000);
+        } else {
+            let pElem = document.createElement('p');
+            pElem.innerHTML = 'Product Added To Basket!';
+            productPage.appendChild(pElem);
+            setTimeout(() => {
+                productPage.removeChild(pElem);
+            }, 3000);
+        }
+    }
 });
